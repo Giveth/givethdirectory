c4d7cdf691e95cb7e62eb13d73732483b331fc64
Jordi Baylina <jordi@baylina.cat>
2017-02-13 18:06:28 +0100

    example2 added

--- env.js
+++ env.js
@@ -23,10 +23,14 @@ const gcb = (err, res) => {
 };
 
 let givethDirectory;
-let vault;
-let miniMeToken;
-let milestoneTracker;
-let givethCampaign;
+let vault = [];
+let miniMeToken = [];
+let milestoneTracker = [];
+let givethCampaign = [];
+
+const now = Math.floor(new Date().getTime() / 1000);
+
+
 
 const escapeCaller = eth.accounts[ 1 ];
 const escapeDestination = eth.accounts[ 2 ];
@@ -34,20 +38,16 @@ const securityGuard = eth.accounts[ 3 ];
 const arbitrator = eth.accounts[ 4 ];
 const donor = eth.accounts[ 5 ];
 const recipient = eth.accounts[ 6 ];
+const reviewer = eth.accounts[ 7 ];
+const milestoneLeadLink = eth.accounts[ 8 ];
 
-const now = Math.floor(new Date().getTime() / 1000);
-
-function deployExample(_cb) {
+function deployCampaign(opts, _cb) {
     const cb = _cb || gcb;
+    let idCampaign;
     async.series([
         (cb1) => {
-            GivethDirectory.deploy(web3, {}, (err, _givethDirectory) => {
-                if (err) {
-                    cb1(err);
-                    return;
-                }
-                givethDirectory = _givethDirectory;
-                console.log("Giveth Directory: " + givethDirectory.contract.address);
+            givethDirectory.contract.numberOfCampaigns((err, res) => {
+                idCampaign = res.toNumber();
                 cb1();
             });
         },
@@ -55,9 +55,9 @@ function deployExample(_cb) {
             GivethCampaign.deploy(
                 web3,
                 {
-                    tokenName: "MiniMe Test Token",
+                    tokenName: opts.tokenName || "MiniMe Test Token",
                     decimalUnits: 18,
-                    tokenSymbol: "MMT",
+                    tokenSymbol: opts.tokenSymbol || "MMT",
                     escapeCaller,
                     escapeDestination,
                     securityGuard,
@@ -65,7 +65,7 @@ function deployExample(_cb) {
                     endFundingTime: now + (86400 * 365 * 30),
                     maximumFunding: web3.toWei(10000),
                     absoluteMinTimeLock: 0,
-                    timeLock: 300,
+                    timeLock: 30,
                     maxSecurityGuardDelay: 86400 * 21,
                 },
                 (err, _givethCampaign) => {
@@ -73,21 +73,21 @@ function deployExample(_cb) {
                         cb1(err);
                         return;
                     }
-                    givethCampaign = _givethCampaign;
-                    console.log("GivethCampaign : " + givethCampaign.contract.address);
+                    givethCampaign[ idCampaign ] = _givethCampaign;
+                    console.log("GivethCampaign : " + _givethCampaign.contract.address);
                     cb1();
                 });
         },
         (cb1) => {
-            givethCampaign.getState((err, st) => {
+            givethCampaign[ idCampaign ].getState((err, st) => {
                 if (err) {
                     cb1(err);
                     return;
                 }
-                miniMeToken = new MiniMeToken(web3, st.tokenAddress);
-                console.log("MiniMeToken : " + miniMeToken.contract.address);
-                vault = new Vault(web3, st.vaultAddress);
-                console.log("Vault : " + vault.contract.address);
+                miniMeToken[ idCampaign ] = new MiniMeToken(web3, st.tokenAddress);
+                console.log("MiniMeToken : " + miniMeToken[ idCampaign ].contract.address);
+                vault[ idCampaign ] = new Vault(web3, st.vaultAddress);
+                console.log("Vault : " + vault[ idCampaign ].contract.address);
                 cb1();
             });
         },
@@ -104,14 +104,14 @@ function deployExample(_cb) {
                         cb1(err);
                         return;
                     }
-                    milestoneTracker = _milestoneTracker;
-                    console.log("MilestoneTracker : " + milestoneTracker.contract.address);
+                    milestoneTracker[ idCampaign ] = _milestoneTracker;
+                    console.log("MilestoneTracker : " + milestoneTracker[ idCampaign ].contract.address);
                     cb1();
                 });
         },
         (cb1) => {
-            vault.contract.authorizeSpender(
-                milestoneTracker.contract.address,
+            vault[ idCampaign ].contract.authorizeSpender(
+                milestoneTracker[ idCampaign ].contract.address,
                 true,
                 {
                     from: eth.accounts[ 0 ],
@@ -119,16 +119,154 @@ function deployExample(_cb) {
                 cb1);
         },
         (cb1) => {
-            givethDirectory.contract.addCampaign("Giveth Test",
-                "Development of Giveth. Donations 3.0",
-                "http://www.giveth.io",
-                miniMeToken.contract.address,
-                vault.contract.address,
-                milestoneTracker.contract.address,
+            givethDirectory.contract.addCampaign(
+                "Giveth Test" || opts.campaignName,
+                "Development of Giveth. Donations 3.0" || opts.campaignDescription,
+                "http://www.giveth.io" || opts.campaignWeb,
+                miniMeToken[ idCampaign ].contract.address,
+                vault[ idCampaign ].contract.address,
+                milestoneTracker[ idCampaign ].contract.address,
                 "", { from: eth.accounts[ 0 ], gas: 300000 }, cb1);
         },
         (cb1) => {
-            givethDirectory.contract.changeStatus(0, 1, { from: eth.accounts[ 0 ] }, cb1);
+            givethDirectory.contract.changeStatus(idCampaign, 1, { from: eth.accounts[ 0 ] }, cb1);
+        },
+    ], cb);
+}
+
+function deployExample(_cb) {
+    const cb = _cb || gcb;
+    async.series([
+        (cb1) => {
+            GivethDirectory.deploy(web3, {}, (err, _givethDirectory) => {
+                if (err) {
+                    cb1(err);
+                    return;
+                }
+                givethDirectory = _givethDirectory;
+                console.log("Giveth Directory: " + givethDirectory.contract.address);
+                cb1();
+            });
+        },
+        (cb1) => {
+            deployCampaign({
+                tokenName: "Token 1",
+                tokenSymbol: "MM1",
+                campaignName: "Campaign 1",
+                campaignDescription: "Desciption 1",
+                campaignWeb: "http://www.giveth.io",
+            }, cb1);
+        },
+    ], cb);
+}
+
+function deployExample2(_cb) {
+    const cb = _cb || gcb;
+    const milestones = [];
+    let hashProposals;
+    async.series([
+        (cb1) => {
+            GivethDirectory.deploy(web3, {}, (err, _givethDirectory) => {
+                if (err) {
+                    cb1(err);
+                    return;
+                }
+                givethDirectory = _givethDirectory;
+                console.log("Giveth Directory: " + givethDirectory.contract.address);
+                cb1();
+            });
+        },
+        (cb1) => {
+            deployCampaign({
+                tokenName: "Giveth Development",
+                tokenSymbol: "MM1",
+                campaignName: "Campaign 1",
+                campaignDescription: "Desciption 1",
+                campaignWeb: "http://www.giveth.io/?idCampaign=1",
+            }, cb1);
+        },
+        (cb1) => {
+            deployCampaign({
+                tokenName: "My Ether Wallet",
+                tokenSymbol: "MM2",
+                campaignName: "Campaign 2",
+                campaignDescription: "Desciption 2",
+                campaignWeb: "http://www.giveth.io/?idCampaign=2",
+            }, cb1);
+        },
+        (cb1) => {
+            deployCampaign({
+                tokenName: "Swarm city",
+                tokenSymbol: "MM3",
+                campaignName: "Campaign 3",
+                campaignDescription: "Desciption 3",
+                campaignWeb: "http://www.giveth.io/?idCampaign=3",
+            }, cb1);
+        },
+        (cb1) => {
+            for (let i = 0; i < 4; i += 1) {
+                milestones.push({
+                    description: "Proposal " + i,
+                    url: "http://www.giveth.io/?idProposal=" + i,
+                    minCompletionDate: now - 86400,
+                    maxCompletionDate: now + (86400 * 3),
+                    reviewer,
+                    milestoneLeadLink,
+                    reviewTime: 86400 * 2,
+                    paymentSource: vault[ 0 ].contract.address,
+                    payData: vault[ 0 ].contract.authorizePayment.getData("Proposal " + i, recipient, web3.toWei(i + 1), 0),
+                    payDescription: "Proposal " + i,
+                    payRecipient: recipient,
+                    payValue: new BigNumber(web3.toWei(i + 1)),
+                    payDelay: 0,
+                });
+            }
+
+            console.log("Propose");
+            milestoneTracker[ 0 ].proposeMilestones({
+                newMilestones: milestones,
+                from: recipient,
+            }, cb1);
+        },
+        (cb1) => {
+            console.log("Get State");
+            milestoneTracker[ 0 ].getState((err, st) => {
+                if (err) {
+                    cb1(err);
+                    return;
+                }
+                hashProposals = st.proposedMilestonesHash;
+                cb1();
+            });
+        },
+        (cb1) => {
+            console.log("Accept");
+            milestoneTracker[ 0 ].acceptProposedMilestones({
+                hashProposals,
+                from: donor,
+                verbose: true,
+            }, cb1);
+        },
+        (cb1) => {
+            console.log("Mark Done 1");
+            milestoneTracker[ 0 ].markMilestoneComplete({
+                idMilestone: 0,
+                from: recipient,
+            }, cb1);
+        },
+        (cb1) => {
+            console.log("Mark Done 2");
+            milestoneTracker[ 0 ].markMilestoneComplete({
+                idMilestone: 1,
+                from: recipient,
+            }, cb1);
+        },
+        (cb1) => {
+            console.log("Approve");
+            milestoneTracker[ 0 ].approveCompletedMilestone({
+                idMilestone: 0,
+                from: reviewer,
+            }, cb1);
         },
     ], cb);
 }
